83592205b93bd767785744ca0c446143fd8b18d6
rudney5000 <rudneyngueko@gmail.com>
2026-03-06 11:17:48 +0300

    feat: add routes favorites and recipe detail page

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../App.tsx";
 import {HomePage} from "../pages/HomePage.tsx";
+import FavoritesPage from "../pages/FavoritesPage.tsx";
+import RecipeDetailPage from "../pages/RecipeDetailPage.tsx";
 
 export const router = createBrowserRouter([
     {
@@ -9,6 +11,14 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <HomePage/>
+            },
+            {
+                path: "/favorites",
+                element: <FavoritesPage/>
+            },
+            {
+                path: "/recipe/:id",
+                element: <RecipeDetailPage/>
             }
         ]
     }
